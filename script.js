b0aca23466e9af1d99323f2ae9135b4f8555d62d
NaiaraSilvaofc <105755105+NaiaraSilvaofc@users.noreply.github.com>
2023-04-04 21:26:53 -0300

    javaScript completo
    javaScript completo

---
+++ script.js
@@ -0,0 +1,52 @@
+//modal 1//
+function showModal() {
+  var element = document.getElementById("modal");
+  element.classList.add("show-modal");
+}
+
+function closeModal() {
+  var element = document.getElementById("modal");
+  element.classList.remove("show-modal");
+}
+//modal 2//
+
+function showModal1() {
+  var element = document.getElementById("modal1");
+  element.classList.add("show-modal1");
+}
+
+function closeModal1() {
+  var element = document.getElementById("modal1");
+  element.classList.remove("show-modal1");
+}
+//modal 3//
+function showModal3() {
+  var element = document.getElementById("modal3");
+  element.classList.add("show-modal3");
+}
+
+function closeModal3() {
+  var element = document.getElementById("modal3");
+  element.classList.remove("show-modal3");
+}
+
+//modal 4//
+function showModal4() {
+  var element = document.getElementById("modal4");
+  element.classList.add("show-modal4");
+}
+
+function closeModal4() {
+  var element = document.getElementById("modal4");
+  element.classList.remove("show-modal4");
+}
+//modalP
+function showModalP() {
+  var element = document.getElementById("modalP");
+  element.classList.add("show-modalP");
+}
+
+function closeModalP() {
+  var element = document.getElementById("modalP");
+  element.classList.remove("show-modalP");
+}
